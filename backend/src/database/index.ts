d23a0b308cdd55a8bd8cc82404aedcb1c55b14c6
David Hernquist <david.hernquist@wbd.com>
2024-09-28 15:49:07 -0400

    adding expiration time string to database

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -6,8 +6,8 @@ const db = new sqlite3.Database(':memory:');
 db.serialize(() => {
   db.run('CREATE TABLE users (id INT, name TEXT)');
   db.run('INSERT INTO users (id, name) VALUES (1, "John Doe")');
-  db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT)');
-  db.run('INSERT INTO jobs (description, requirements, name, email, phone) VALUES ("Job description", "Job requirements", "John Poster", "j.poster@mail.com", "123456789")');
+  db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT, expiration TEXT)');
+  db.run('INSERT INTO jobs (description, requirements, name, email, phone, expiration) VALUES ("Job description", "Job requirements", "John Poster", "j.poster@mail.com", "123456789", "2024-10-1 20:14:09.123")');
 })
 
 export function getUsers(callback: any) {
@@ -35,7 +35,7 @@ export function getJob(id: number, callback: any) {
     db.all(query, [id], callback)
 }
 
-export function addJob({ description, requirements, name, email, phone }: { description: string, requirements: string, name: string, email: string, phone: string }, callback: any) {
-    const query = 'INSERT INTO jobs (description, requirements, name, email, phone) VALUES (?, ?, ?, ?, ?)';
-    db.run(query, [description, requirements, name, email, phone], callback)
+export function addJob({ description, requirements, name, email, phone, expiration }: { description: string, requirements: string, name: string, email: string, phone: string, expiration: string}, callback: any) {
+    const query = 'INSERT INTO jobs (description, requirements, name, email, phone, expiration) VALUES (?, ?, ?, ?, ?, ?)';
+    db.run(query, [description, requirements, name, email, phone, expiration], callback)
 }
