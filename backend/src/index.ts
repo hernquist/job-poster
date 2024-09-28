dcc7e93e337ba04b9454aa2f31f25bc2a5d5e0a7
David Hernquist <david.hernquist@wbd.com>
2024-09-28 14:47:52 -0400

    adding new jobs route group

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import usersRoutes from './routes/users';
+import jobsRoutes from './routes/jobs';
 
 const app = express();
 app.use(express.json())
@@ -8,6 +9,7 @@ const port = 3001;
 
 // routing
 app.use('/users', usersRoutes);
+app.use('/jobs', jobsRoutes);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);
