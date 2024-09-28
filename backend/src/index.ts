527c53106d8d3de825d3095cf0ac2ebf92f34879
David Hernquist <david.hernquist@wbd.com>
2024-09-28 12:37:35 -0400

    add "0.0.0.0" to have listen to docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -57,6 +57,6 @@ app.delete('/users/:id', (req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
