d368e6f1ad489a7d11192cf6abaf48fc6a3a3584
David Hernquist <david.hernquist@wbd.com>
2024-09-28 14:48:24 -0400

    create getJobs in database

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -6,6 +6,8 @@ const db = new sqlite3.Database(':memory:');
 db.serialize(() => {
   db.run('CREATE TABLE users (id INT, name TEXT)');
   db.run('INSERT INTO users (id, name) VALUES (1, "John Doe")');
+  db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT)');
+  db.run('INSERT INTO jobs (description, requirements, name, email, phone) VALUES ("Job description", "Job requirements", "John Poster", "j.poster@mail.com", "123456789")');
 })
 
 export function getUsers(callback: any) {
@@ -21,5 +23,10 @@ export function deleteUser(id: number, callback: any) {
 export function addUser(id: number, name: string, callback: any) {
     const query = 'INSERT INTO users (id, name) VALUES (?, ?)';
     db.run(query, [id, name], callback)
+}
+
+export function getJobs(callback: any) {
+    const query = 'SELECT * FROM jobs';
+    db.all(query, [], callback)
 }
 
