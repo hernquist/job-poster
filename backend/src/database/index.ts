1329057575872d4d90d20639a5aee827caf38e10
David Hernquist <david.hernquist@wbd.com>
2024-09-28 16:21:02 -0400

    added bids and put in some stubbed out data

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -9,7 +9,7 @@ db.serialize(() => {
   db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT, expiration TEXT)');
   db.run('INSERT INTO jobs (description, requirements, name, email, phone, expiration) VALUES ("Job description", "Job requirements", "John Poster", "j.poster@mail.com", "123456789", "2024-10-1 20:14:09.123")');
   db.run('CREATE TABLE bids (id INTEGER PRIMARY KEY AUTOINCREMENT, job_id INT, user_id INT, amount INT, FOREIGN KEY(job_id) REFERENCES jobs(id))');
-  db.run('INSERT INTO bids (job_id, user_id, amount) VALUES (1, 1, 100), (1, 1, 200), (1, 2, 150)');
+  db.run('INSERT INTO bids (job_id, user_id, amount) VALUES (1, 1, 100), (1, 1, 200), (1, 2, 150), (2, 1, 300)');
 })
 
 export function getUsers(callback: any) {
@@ -40,4 +40,9 @@ export function getJob(id: number, callback: any) {
 export function addJob({ description, requirements, name, email, phone, expiration }: { description: string, requirements: string, name: string, email: string, phone: string, expiration: string}, callback: any) {
     const query = 'INSERT INTO jobs (description, requirements, name, email, phone, expiration) VALUES (?, ?, ?, ?, ?, ?)';
     db.run(query, [description, requirements, name, email, phone, expiration], callback)
+}
+
+export function getBids(jobId: number, callback: any) {
+    const query = 'SELECT * FROM bids WHERE job_id = ?';
+    db.all(query, [jobId], callback)
 }
