4726536b7130c1991b542e8961a85f9147430d1d
David Hernquist <david.hernquist@wbd.com>
2024-09-28 23:31:02 -0400

    building out ShowJobs view

--- backend/src/database/types.ts
+++ backend/src/database/types.ts
@@ -1,1 +1,9 @@
-export interface IJob { title: string, description: string, requirements: string, name: string, email: string, phone: string, expiration: string};
+export interface IJob { 
+    title: string, 
+    description: string, 
+    requirements: string, 
+    name: string, 
+    email: string, 
+    phone: string, 
+    expiration: string
+};
