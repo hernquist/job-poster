525c21c327ae789f054dafe31b7d31f6c5acb9b9
David Hernquist <david.hernquist@wbd.com>
2024-09-29 00:28:30 -0400

    expand job to include timeCreated

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,20 +1,14 @@
 import sqlite3 from 'sqlite3';
-import { IJob } from './types';
+import { IJobDB } from './types';
+import seededData from './seeds';
 
 // Create and setup the SQLite database
 const db = new sqlite3.Database(':memory:');
-
-const seededData = {
-    users: `VALUES (1, "John Doe"), (2, "Jane Doe")`,
-    jobs: `VALUES ("Seeded job", "Job description", "Job requirements", "John Poster", "j.poster@mail.com", "123456789", "2024-10-1 20:14:09.123")`,
-    bids: `VALUES (1, 1, 100), (1, 1, 200), (1, 2, 150), (2, 1, 300)`
-};
-
 db.serialize(() => {
   db.run('CREATE TABLE users (id INT, name TEXT)');
   db.run('INSERT INTO users (id, name) ' + seededData.users);
-  db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT, expiration TEXT)');
-  db.run('INSERT INTO jobs (title, description, requirements, name, email, phone, expiration) ' + seededData.jobs);
+  db.run('CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, requirements TEXT, name TEXT, email TEXT, phone TEXT, expiration TEXT, timeCreated TEXT)');
+  db.run('INSERT INTO jobs (title, description, requirements, name, email, phone, expiration, timeCreated) ' + seededData.jobs);
   db.run('CREATE TABLE bids (id INTEGER PRIMARY KEY AUTOINCREMENT, job_id INT, user_id INT, amount INT, FOREIGN KEY(job_id) REFERENCES jobs(id))');
   db.run('INSERT INTO bids (job_id, user_id, amount) ' + seededData.bids);
 })
@@ -44,9 +38,9 @@ export function getJob(id: number, callback: any) {
     db.all(query, [id], callback)
 }
 
-export function addJob({ title, description, requirements, name, email, phone, expiration }: IJob, callback: any) {
-    const query = 'INSERT INTO jobs (title, description, requirements, name, email, phone, expiration) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    db.run(query, [title, description, requirements, name, email, phone, expiration], callback)
+export function addJob({ title, description, requirements, name, email, phone, expiration, timeCreated }: IJobDB, callback: any) {
+    const query = 'INSERT INTO jobs (title, description, requirements, name, email, phone, expiration, timeCreated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    db.run(query, [title, description, requirements, name, email, phone, expiration, timeCreated], callback)
 }
 
 export function getBidsByJobId(jobId: number, callback: any) {
