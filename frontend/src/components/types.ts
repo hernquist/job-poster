b196e7ebf066977ebeeeb8a5143d1f8e1725bfbf
David Hernquist <david.hernquist@wbd.com>
2024-09-29 00:34:15 -0400

    adding typing around Jobs

---
+++ frontend/src/components/types.ts
@@ -0,0 +1,11 @@
+export interface IJob { 
+    id: number,
+    title: string, 
+    description: string, 
+    requirements: string, 
+    name: string, 
+    email: string, 
+    phone: string, 
+    expiration: string,
+    timeCreated: string
+};
