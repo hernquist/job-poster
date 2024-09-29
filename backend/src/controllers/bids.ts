e527af12fc14926b8a5b0ca4b8ae601ba26c8c52
David Hernquist <david.hernquist@wbd.com>
2024-09-29 14:21:35 -0400

    add bids logic to controller

---
+++ backend/src/controllers/bids.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { addBid, getJob } from '../database';
+
+const controller = { 
+    addBid: (req: Request, res: Response) => {
+        const { jobId, amount } = req.body;
+        if (typeof amount === "undefined") {
+            res.status(400).json({ error: 'Please provide a job bid amount' });
+            return;
+        }
+    
+        // make sure job exists before allowing bid to be added
+        getJob(jobId, (err: { message: any; }, job: any) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }//
+            if (job.length === 0) {
+                res.status(404).json({ error: `Job ${jobId} not found for bid` });
+                return
+            } 
+        })
+    
+        addBid(jobId, amount, (err: { message: any; }) => {
+            if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+            }
+            res.status(200).json({ message: 'Job added' });
+        });
+    }
+}
+
+export default controller;
