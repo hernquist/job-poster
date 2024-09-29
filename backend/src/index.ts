0ab1ba8fec7a9d274d8933abd20e424eae926f0f
David Hernquist <david.hernquist@wbd.com>
2024-09-28 23:00:52 -0400

    account for bids route for post

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import usersRoutes from './routes/users';
 import jobsRoutes from './routes/jobs';
+import bidsRoutes from './routes/bids';
 
 const app = express();
 
@@ -14,6 +15,7 @@ const port = 3001;
 // routing
 app.use('/users', usersRoutes);
 app.use('/jobs', jobsRoutes);
+app.use('/bids', bidsRoutes)
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);
