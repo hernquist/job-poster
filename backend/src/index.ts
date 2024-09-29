296ba6f4fea53ff160aac26468591913a5a6edd4
David Hernquist <david.hernquist@wbd.com>
2024-09-28 21:40:23 -0400

    adding cors workaround for localhost in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import usersRoutes from './routes/users';
 import jobsRoutes from './routes/jobs';
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 const port = 3001;
