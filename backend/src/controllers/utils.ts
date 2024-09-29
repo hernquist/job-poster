1e70dc72a0701aadb38f0748c1bedd14b599862b
David Hernquist <david.hernquist@wbd.com>
2024-09-29 14:22:49 -0400

    pulled out complicated join and builld logic

---
+++ backend/src/controllers/utils.ts
@@ -0,0 +1,31 @@
+function getBidsFromJobs(jobs: any[]) {
+    return jobs?.reduce((bidsById: any, job: any) => {
+        if (job.bidsAmount === null) {
+            return bidsById;
+        }
+        if (bidsById?.[job.jobId]) {
+            bidsById[job.jobId].push({amount: job.bidsAmount, userId: job.bidsUserId, id: job.bidsId, timeCreated: job.bidsTimeCreated})
+        } else {
+            bidsById[job.jobId] = [{amount: job.bidsAmount, userId: job.bidsUserId, id: job.bidsId, timeCreated: job.bidsTimeCreated}]
+        }
+        return bidsById;
+    }, {});
+}
+
+// problably better to use knex to organize the jobs
+function applyBidsToJobs (jobs: any[]) {
+    const getBids = getBidsFromJobs(jobs);
+
+    return jobs?.filter((job: any, index: number) => jobs.findIndex( (j: any) => j.jobId === job.jobId ) === index).map((job: any) => {
+        return {
+            id: job.jobId,
+            title: job.title,
+            requirements: job.requirements,
+            name: job.name,
+            email: job.email,
+            bids: getBids[job.jobId]
+        }
+    });
+};
+
+export { applyBidsToJobs };
