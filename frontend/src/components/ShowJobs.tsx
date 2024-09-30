5ea9dfe754a2371ea1633e4f267b7f558425cdb5
David Hernquist <david.hernquist@wbd.com>
2024-09-29 23:55:35 -0400

    move around line to group concepts together

--- frontend/src/components/ShowJobs.tsx
+++ frontend/src/components/ShowJobs.tsx
@@ -3,7 +3,6 @@ import { useLocation } from 'react-router-dom';
 import JobList from "./JobList";
 import { jobDataFunctionMap } from "./utils";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const styles = `
 .show-jobs-container {
     margin: 20px;
@@ -17,27 +16,30 @@ h5 {
   margin: 0 auto 12px; 
 }`;
 
-export function ShowJobs({title}: {title: string}) {
-    const { pathname} = useLocation();
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-    const {
-      data: jobs,
-      error,
-      isValidating,
-    } = useSWR('http://localhost:3001/jobs', fetcher);
+export function ShowJobs({title}: {title: string}) {
+  const { pathname} = useLocation();
+
+  const {
+    data: jobs,
+    error,
+    isValidating,
+  } = useSWR('http://localhost:3001/jobs', fetcher);
+
+  // Handles error and loading state
+  if (error) return <div className='failed'>failed to load</div>;
+  if (isValidating || !pathname) return <div className="Loading">Loading...</div>;
+
+  const jobDataSortingFunction = jobDataFunctionMap[pathname as keyof typeof jobDataFunctionMap];
   
-    const jobDataSortingFunction = jobDataFunctionMap[pathname as keyof typeof jobDataFunctionMap];
-    
-    // Handles error and loading state
-    if (error) return <div className='failed'>failed to load</div>;
-    if (isValidating || !pathname) return <div className="Loading">Loading...</div>;
   return (
     <>
-    <style dangerouslySetInnerHTML={{__html: styles}} />
-    <div className="show-jobs-container">
-      <h5>{title}</h5>
-      <JobList jobs={jobs.sort(jobDataSortingFunction).slice(0,5)} />
-    </div>
+      <style dangerouslySetInnerHTML={{__html: styles}} />
+      <div className="show-jobs-container">
+        <h5>{title}</h5>
+        <JobList jobs={jobs.sort(jobDataSortingFunction).slice(0,5)} />
+      </div>
     </>
   );
 }
