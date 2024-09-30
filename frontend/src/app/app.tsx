f0d136038320e14849dc27f38eb4fe3e3a8755b7
David Hernquist <david.hernquist@wbd.com>
2024-09-29 21:50:35 -0400

    add styles to app file

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,9 +2,19 @@
 import NavBar from "../components/NavBar";
 import Router from "./Router";
 
+const styles = `
+* {
+    box-sizing: border-box;
+    font-family: Arial, sans-serif;
+    font-size: 16px;
+    background-color: #f0f0f0;
+}
+`;
+
 function App() {
   return (
     <>
+      <style  dangerouslySetInnerHTML={{__html: styles }} />
       <NavBar/>
       <Router />
     </>
