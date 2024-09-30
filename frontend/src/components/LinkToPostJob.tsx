e5af6434a73546a5f665d26e54a5f9ec88917f7f
David Hernquist <david.hernquist@wbd.com>
2024-09-29 21:52:07 -0400

    add styles to LinkToPostJob component

--- frontend/src/components/LinkToPostJob.tsx
+++ frontend/src/components/LinkToPostJob.tsx
@@ -1,7 +1,23 @@
 import { Link } from 'react-router-dom';
+import { RoutingConfig } from '../app/routing-config';
+
+const styles = `
+.link {
+    margin: 20px;
+    padding: 20px;
+    background-color: lightblue;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    text-decoration: none;
+    color: black;
+}
+`;
 
 export function LinkToPostJob( ) {
     return (
-        <Link to="/post-job">CLICK HERE TO POST A NEW JOB!</Link>
+        <>
+            <style dangerouslySetInnerHTML={{__html: styles}} />
+            <Link className="link" to={RoutingConfig.postJob}>CLICK HERE TO POST A NEW JOB!</Link>
+        </>
     )
 }
