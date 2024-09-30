a33fc17e5863212e171d98259f6e077e9bfbc328
David Hernquist <david.hernquist@wbd.com>
2024-09-29 21:51:46 -0400

    add styles to JobList component

--- frontend/src/components/JobList.tsx
+++ frontend/src/components/JobList.tsx
@@ -1,12 +1,20 @@
 import ShowJob from "./ShowJob";
 import { IJob } from "./types";
 
+const styles = `
+.job-list {
+    max-width: 300px;
+}`;
+
 export default function JobList({ jobs }: { jobs: IJob[] }) {
     return (
-        <div>
-            {jobs.map((job) => (
-                <ShowJob key={job.id} job={job} />
-            ))}
-        </div>
+        <>
+            <style dangerouslySetInnerHTML={{__html: styles }} />
+            <div className="job-list">
+                {jobs.map((job) => (
+                    <ShowJob key={job.id} job={job} />
+                ))}
+            </div>
+        </>
     );
 }
