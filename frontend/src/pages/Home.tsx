9c68138f73c06305214196d9021a665498b20447
David Hernquist <david.hernquist@wbd.com>
2024-09-29 21:59:09 -0400

    make Home component part of dynamic child

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { LinkToPostJob } from '../components/LinkToPostJob';
 import { ShowJobs } from '../components/ShowJobs';
-import styles from './app.module.less';
 
-export function Home() {
-
+function Home() {
   return (
     <div>
-        <ShowJobs/>
-        <LinkToPostJob />
+      <ShowJobs title="5 Most Recent Jobs"/>
+      <LinkToPostJob />
     </div>
   );
 }
