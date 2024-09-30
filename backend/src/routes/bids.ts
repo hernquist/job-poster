c860e839246c98e56f1c23f0c25bce5a788bfca1
David Hernquist <david.hernquist@wbd.com>
2024-09-29 21:49:44 -0400

    remove bid routes file

--- backend/src/routes/bids.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import controller from '../controllers/bids';
-
-const router = express.Router();
-
-// API route to add a bid
-router.post('', controller.addBid);
-
-export default router;
