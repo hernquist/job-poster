acdc78ebb415d18f27383c6bbea03eddb1143375
David Hernquist <david.hernquist@wbd.com>
2024-09-30 23:38:23 -0400

    add a css to jazz up card

--- frontend/src/components/ShowJob.tsx
+++ frontend/src/components/ShowJob.tsx
@@ -4,18 +4,25 @@ import { getLowestBid } from "./utils";
 
 const styles = `
 .job-card {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    width: 100%;
-    border-radius: 6px;
-    border: 1px solid #000;
-    padding: 6px;
-    margin: 6px;
-    font-size: 1.2em;
-    font-family: Arial, sans-serif;
-    background-color: peachpuff;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: calc(100% - 20px);
+  border-radius: 6px;
+  border: 1px solid #000;
+  padding: 6px;
+  margin: 10px;
+  font-size: 1.2em;
+  font-family: Arial, sans-serif;
+  background-color: peachpuff;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
 }
+
+.job-card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
 
 .job-card__bids-container {
     display: flex;
@@ -33,6 +40,19 @@ const styles = `
 .job-card__text {
     margin: 2px;
     padding: 2px;
+}
+
+.job-card__link {
+    text-align: center;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    text-decoration: none;
+}
 `;
 
 export default function ShowJob({ job, hidePostBid = false }: { job: IJob, hidePostBid?: boolean }) {
@@ -43,14 +63,17 @@ export default function ShowJob({ job, hidePostBid = false }: { job: IJob, hideP
         <h4 className="job-card__title">{job.title}</h4>
         <p className="job-card__text">{job.description}</p>
         <p className="job-card__text">Requirements: {job.requirements}</p>
+        <p className="job-card__text">Job exprires: ${job.expiration}</p>
         <p className="job-card__text">Post By: {job.name}</p>
+        <p className="job-card__text">Phone: {job.phone}</p>
+        <p className="job-card__text">Email: {job.email}</p>
         <p className="job-card__text">Date Posted: {new Date(job.timeCreated).toDateString()}</p>
         <div className="job-card__bids-container">
           <p className="job-card__text">{job?.bids?.length ? `# of bids: ${job?.bids?.length}` : `no bids`}</p> 
           <p className="job-card__text">{getLowestBid(job.bids)}</p>
         </div>
         {!hidePostBid && 
-          <Link to={`/job/${job.id}`} state={{job}}>Make A Bid</Link>
+          <Link className="job-card__link" to={`/job/${job.id}/bid`} state={{job}}>Make A Bid</Link>
         }
       </div>
     </>
